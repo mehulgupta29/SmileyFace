e59899043b32657692202013b23084f7e1b49beb
Mehul Gupta <MehulGupta@Mehuls-MBP.fios-router.home>
2016-10-10 15:41:29 -0400

    Initial commit

---
+++ SmileyFace/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  SmileyFace
+//
+//  Created by Mehul Gupta on 10/10/16.
+//  Copyright © 2016 Mehul Gupta. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
